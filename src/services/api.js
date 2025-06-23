dbc9ac25526ef44fe47be51e21bb9242d26787be
jmuchiri <mmjames451@gmail.com>
2025-06-23 23:25:58 +0300

    update:check for unathorized and server error response codes

--- src/services/api.js
+++ src/services/api.js
@@ -1,5 +1,5 @@
-const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
-const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+const TMDB_API_KEY = process.env.NEXT_PUBLIC_TMDB_API_KEY;
+const OMDB_API_KEY = process.env.NEXT_PUBLIC_OMDB_API_KEY;
 
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const OMDB_BASE_URL = 'http://www.omdbapi.com';
@@ -11,6 +11,10 @@ const fetchJson = async(url) => {
         if (!response.ok) {
             if (response.status == 429) {
                 throw new Error("Too many requests")
+            } else if (response.status == 401) {
+                throw new Error("Authaorized, please incluse api key")
+            } else if (response.status == 500) {
+                throw new Error("server error")
             }
             throw new Error("An issue occured")
         }
