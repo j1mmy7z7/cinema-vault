6049330235745e44fdd3ba02a1e7b8bd4c06b6af
jmuchiri <mmjames451@gmail.com>
2025-06-23 22:22:06 +0300

    feature: setup service file containign the api file , setup our api keys with the base url

---
+++ src/services/api.js
@@ -0,0 +1,6 @@
+const TMDB_API_KEY = process.env.REACT_APP_TMDB_API_KEY;
+const OMDB_API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
+const OMDB_BASE_URL = 'http://www.omdbapi.com';
+
