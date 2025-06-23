02c6275d144dff6549841cc4cfd7fe93745a5c59
jmuchiri <mmjames451@gmail.com>
2025-06-23 23:25:22 +0300

    chore: test our fetch function

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,8 +1,34 @@
+'use client';
+import { useEffect } from 'react';
+import { tmdbService } from '../services/api'; 
 
-export default function Home() {
-  return (
-    <>
-    <h1> content goes here ... </h1>
-    </>
-  );
+function App() {
+  useEffect(() => {
+    const test = async () => {
+      console.log("we are doing something at least");
+      const data = await tmdbService.search("fight club");
+      console.log("TMDB Data:", data);
+    };
+
+    test();
+  }, []);
+
+  return <div>App Loaded</div>;
 }
+
+export default App;
+
+
+
+
+
+
+
+
+  // export default function App() {
+//   return (
+//     <>
+//     <h1> content goes here ... </h1>
+//     </>
+//   );
+// }
