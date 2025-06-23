300e89bbc7ce918a7172c54a2a0284b7599b3be7
jmuchiri <mmjames451@gmail.com>
2025-06-24 00:38:27 +0300

    update: functions to add movies to the watchlist

--- src/utils/storage.js
+++ src/utils/storage.js
@@ -43,5 +43,23 @@ export const watchlistManager = {
     return storage.get(STORAGE_KEYS.WATCHLIST) || [];
   },
 
-  
+  addToWatchlist: (movie) => {
+    const watchlist = watchlistManager.getWatchlist();
+    const isAlreadyAdded = watchlist.some(item => item.id === movie.id);
+    
+    if (!isAlreadyAdded) {
+      const newWatchlist = [...watchlist, {
+        id: movie.id,
+        title: movie.title || movie.name,
+        poster_path: movie.poster_path,
+        media_type: movie.media_type || 'movie',
+        release_date: movie.release_date || movie.first_air_date,
+        vote_average: movie.vote_average,
+        added_date: new Date().toISOString()
+      }];
+      storage.set(STORAGE_KEYS.WATCHLIST, newWatchlist);
+      return true;
+    }
+    return false;
+  },
 }
